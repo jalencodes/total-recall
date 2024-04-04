ec0588d1dd94e5ea7b7e0a505b04fd3aa085e742
Jalen Vaughn <jalencodes@outlook.com>
2024-04-04 14:56:12 -0400

    Part A done

---
+++ script.js
@@ -0,0 +1,7 @@
+// Part A
+let myName              = 'Jalen'
+let favoriteLanguage    = 'Python'
+let newFavoriteLanguage = 'Javascript'
+favoriteLanguage = newFavoriteLanguage
+console.log(`My name is ${myName} and my favorite coding language is ${favoriteLanguage}`);
+
