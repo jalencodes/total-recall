9368362768302afa962ba803a3d846b5f73a1a95
Jalen Vaughn <jalencodes@outlook.com>
2024-04-09 10:19:28 -0400

    Part 1 is complete, Part A and B of loops complete

--- script.js
+++ script.js
@@ -35,3 +35,58 @@ console.log(e === 'Kevin');
 console.log(a + b === c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
 console.log(a + a < d); // note: the answer is a simple arithmetic equation, not something "weird"
 console.log(48 == '48');
+
+// Part D
+let animal = 'cow'
+
+if (animal == 'cow')
+{
+    console.log('mooooo');
+}
+else 
+{
+    console.log("Hey! You're not a cow");
+}
+
+// Part E
+let age = 15
+if (age >= 16){
+    console.log("Here are the keys!");
+}
+else{
+    console.log("Sorry, you're too young.");
+}
+
+/*
+Loops
+*/
+
+//Part A
+console.log("Part A\n");
+for(let i = 0; i <= 10; i++)
+{
+    console.log(i);
+}
+console.log("\n\n");
+for(let i = 10; i <= 400; i++)
+{
+    console.log(i);
+}
+console.log("\n\n");
+for(let j = 12; j <= 400; j += 3)
+{
+    console.log(j);
+}
+
+//Part B
+console.log("Part B\n");
+for(let i = 1; i < 101; i++)
+{
+    if (i%2 == 0)
+    {
+        console.log(i + " is an even number");
+    }
+    else{
+        console.log(i);
+    }
+}
