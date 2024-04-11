afc741d7c7737bf97fe1b080a46fe25c5cb28c19
Jalen Vaughn <jalencodes@outlook.com>
2024-04-11 15:46:33 -0400

    Object questions completed

--- script.js
+++ script.js
@@ -127,8 +127,39 @@ console.log(`You have $${bankAccount} in your bank account`);
 
 console.log('\n3. Arrays & Control flow\n\n');
 
+//Part B
+console.log("\nPart B");
+const quotes = ['"','"','"']
+
+//Part C
+console.log("\nPart C");
+const randomThings = [1, 10, "Hello", true]
+let first = randomThings[0]
+randomThings[2] = 'World'
+console.log(randomThings)
+
+//Part D
+console.log("\nPart D");
+const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"];
+third = ourClass[2]
+ourClass[4] = 'Octocat'
+ourClass.push('Cloud City')
+console.log(ourClass);
+
+//Part E
+console.log("\nPart E");
+const myArray = [5, 10, 500, 20]
+myArray.push('Aegon', 'Tpday')
+console.log(myArray);
+myArray.shift()
+myArray.unshift('Bob Marley')
+console.log(myArray);
+myArray.pop()
+newArray = myArray.reverse()
+console.log(newArray);
+
 //Part F
-console.log("Part F\n");
+console.log("\nPart F");
 let num = 8
 if (num < 100)
 {
@@ -139,7 +170,7 @@ else {
 }
 
 //Part G
-console.log("\nPart G\n");
+console.log("\nPart G");
 if (num < 5)
 {
     console.log(`${num} is a little number`);
@@ -153,3 +184,113 @@ else
     console.log(`${num} is a monkey`);
 }
 
+// Part H
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "Per Scholas hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+
+const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+let kristyn_outfit = `Kristyn is rocking that ${kristynsCloset[2]} today!`
+console.log(kristyn_outfit);
+kristynsCloset.splice(6, 0, 'raybans')
+console.log(kristynsCloset);
+kristynsCloset[5] = 'stained knit hat'
+console.log(kristynsCloset);
+firstItem = thomsCloset[0][0]
+console.log(firstItem);
+pants = thomsCloset[1][1]
+console.log(pants);
+let accessory = thomsCloset[2][2] 
+console.log(accessory);
+message = `Thom is looking fierce in a ${firstItem}, ${pants}, and ${accessory}!`
+console.log(message);
+
+
+// Objects
+console.log('\nOBJECTS');
+
+console.log("\nPart A");
+const user = {
+    name: 'tommy rons',
+    email: 'tommylink@gmail.com',
+    age: 22,
+    purchased: []
+}
+console.log(user);
+
+console.log("\nPart B");
+user.email = 'tommynew@gamil.com'
+user.age++
+console.log(user);
+
+console.log("\nPart C");
+user.location = 'Emerald City'
+console.log(user);
+
+console.log("\nPart D");
+user.purchased.push('carbohydrates', 'peace of mind', 'Merino jodhpurs')
+console.log(user);
+console.log(user.purchased[2]);
+
+console.log("\nPart E");
+user.friend = {
+    name: "Sidney Loppins",
+    age: 45,
+    location: 'Munchkin Country',
+    purchased: []
+}
+console.log(user.friend.name);
+console.log(user.friend.location);
+user.friend.age = 55;
+user.friend.purchased.push('The One Ring', 'A Latte')
+console.log(user.friend.purchased[1]);
+// console.log(user);
+
+console.log("\nPart F");
+console.log(`${user.name} purchased:`);
+for(let item of user.purchased) {console.log(item);}
+console.log(`\n${user.friend.name} purchased:`);
+for(let item of user.friend.purchased) {console.log(item);}
+
+console.log("\nPart G");
+
+function updateUser()
+{
+    user.age++
+    user.name = user.name.toLocaleUpperCase()
+}
+
+function oldAndLoud(userObject)
+{
+    userObject.age++
+    userObject.name = userObject.name.toLocaleUpperCase()
+}
+updateUser()
+console.log(user);
+oldAndLoud(user)
+console.log(user);
